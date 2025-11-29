5b5df0f85aea39baab7341dde4b3fd787c03f75e
BeyondBitsLtda <beyondbitsltda@gmail.com>
2025-11-29 20:20:38 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const contactConfig = {
     cc: 'aGVucmlxdWUuYXJhdWpvQGNvcmVkYi5jb20uYnI=',
   },
   whatsapp: {
-    phone: 'KzU1IDM4IDk4NjItMjQyOA==', // base64 de 553988622428
+    phone: 'NTUzODk5ODYyMjQyOA==', // base64 de 553988622428
   },
 };
 
